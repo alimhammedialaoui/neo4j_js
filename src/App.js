5abcee9e917ec1e263851b9c0a4f63169af5f697
oussiw <ous.siw2@gmail.com>
2021-01-16 19:48:02 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 import Home from "./screens/Home";
 
 function App() {
   return (
-    <div className="App">
-      <Home/>
+    <div className="container">
+        <h3>Neo4j App</h3><hr/>
+        <Home/>
     </div>
   );
 }
