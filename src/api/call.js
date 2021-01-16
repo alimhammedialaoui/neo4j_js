4d3a0e5f2322cdbcdc7fdb91467c294ceb456e57
oussiw <ous.siw2@gmail.com>
2021-01-16 21:21:41 +0100

    commit

--- src/api/call.js
+++ src/api/call.js
@@ -35,11 +35,6 @@ class Api {
     }
 
     getUsagers = () => {
-        console.log("Get")
-        // const neo4j = require('neo4j-driver');
-        // const driver = neo4j.driver('bolt://localhost:7687',
-        //     neo4j.auth.basic('neo4j', 'Oussama2'),
-        //     {/* encrypted: 'ENCRYPTION_OFF' */});
         var usagers=[];
         const query = `MATCH (n:Usager)-[r]->(m) return distinct n as usager`;
 
