1e44dd70e8a304c0f233bd01069fe7dbc6833c32
alilou1998 <alimalaoui3@gmail.com>
2021-01-17 00:46:36 +0100

    formMoyenDeTransport

--- src/component/MoyenDeTransport.js
+++ src/component/MoyenDeTransport.js
@@ -22,10 +22,10 @@ class MoyenDeTransport extends Component {
             <div>
                <h3>Adnane M'barki is the best</h3><hr/>
                 <button className="btn btn-outline-info" onClick={()=>this.setState({
-                    formtransportHidden:true
+                    formtransportHidden:!this.state.formtransportHidden
                 })}>Add Transport</button>
                 <hr/>
-                {!this.state.formtransportHidden && <FormMoyenDeTransport/>}
+                {this.state.formtransportHidden && <FormMoyenDeTransport/>}
             </div>
         );
     }
