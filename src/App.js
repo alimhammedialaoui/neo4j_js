cf96fc41d0fd7ce5b930333ebbd071a94ceb39fe
Adnane Mbarki <mbarki.adnane98@gmail.com>
2021-01-17 02:05:27 +0100

    My commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import './App.css';
-import Usager from './component/Usager'
-import moyenDeTransport from './component/MoyenDeTransport'
+import Usager from './component/Usager';
+import MoyenDeTransport from './component/MoyenDeTransport';
+import {Component} from 'react'
 // import Home from "./screens/Home";
-function App() {
-  return (
+class App extends Component {
+  render(){return (
     <div className="container">
         <h3>Neo4j App</h3><hr/>
         <Usager/>
+        
+        <MoyenDeTransport/>
     </div>
     )
   }
-
+}
 export default App;
--- src/component/UserPreview.js
+++ src/component/UserPreview.js
@@ -47,7 +47,7 @@ class UserPreview extends Component{
     getUsagers = () => {
         const neo4j = require('neo4j-driver')
 
-        const driver = neo4j.driver("bolt://localhost:7687", neo4j.auth.basic("neo4j", "0000"))
+        const driver = neo4j.driver("bolt://localhost:7687", neo4j.auth.basic("neo4j", "1234"))
         const session = driver.session({database: "neo4j"});
         const query = `MATCH (n:Usager)-[r]->(m) return distinct n as usager`;
 
